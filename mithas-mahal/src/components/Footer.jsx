a0d00314682e08a546ae3c2eb108eed2a60870ad
sivapriyaa17 <sivapriyaa2006@gmail.com>
2025-08-16 23:06:02 +0530

    Update Footer.jsx

--- mithas-mahal/src/components/Footer.jsx
+++ mithas-mahal/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white text-center py-8">
-      <p>&copy; 2025 Mithas Mahal Sweets. All rights reserved.</p>
+      <p>&copy; 2025 Mithas Mahal. All rights reserved.</p>
       <p><b>Address:</b> 123 Sweet Street, Chennai, Tamil Nadu, India</p>
       <p><b>Phone:</b> +91 98765 43210 | <b>Email:</b> contact@mithassweets.com</p>
     </footer>
