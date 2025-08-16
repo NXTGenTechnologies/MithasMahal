376221579493b8385a4b746b112b6673ae05feb1
sivapriyaa17 <sivapriyaa2006@gmail.com>
2025-08-16 23:16:27 +0530

    Update Products.jsx

--- mithas-mahal/src/components/Products.jsx
+++ mithas-mahal/src/components/Products.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
-import laddu from '../assets/images/laddu.jpg';
-import rasu from '../assets/images/rasu.jpg';
-import soan from '../assets/images/soan.jpg';
-import kaju from '../assets/images/kaju.jpg';
-import mys from '../assets/images/mys.jpg';
-
+import laddu from '/images/sweets/laddu.webp';
+import rasu from '/images/rasugulla.webp';
+import soan from '/images/soan-papdi.webp';
+import kaju from '/images/kajukatli.webp';
+import mys from '/images/mysorepak.webp';
+import dal from '/images/savouries/DaalSamosa.webp'
+import nendram from '/images/savouries/NendramChips.webp'
+import spicy from '/images/savouries/SpicyMurkku.webp'
+import thathai from '/images/savouries/Thatthai.webp'
+import cream from '/images/bakes/CreamRoll.webp'
+import ghee from '/images/bakes/Ghee-Cake.webp'
+import banana from '/images/bakes/BANANACAKE.webp'
+import jam from '/images/bakes/jam-roll.webp'
 const products = [
   { id: 1, name: 'Motichoor Laddu', price: 'Rs 200', img: laddu },
   { id: 2, name: 'Rasgulla', price: 'Rs 180', img: rasu },
   { id: 3, name: 'Soan Papadi', price: 'Rs 99', img: soan },
   { id: 4, name: 'Kaju katli', price: 'Rs 140', img: kaju },
   { id: 5, name: 'Mysore pak', price: 'Rs 110', img: mys },
+  { id: 6, name: 'Daal Samosa', price: 'Rs 120', img: dal },
+  { id: 7, name: 'Nendram Chips', price: 'Rs 90', img: nendram },
+  { id: 8, name: 'Spicy Murukku', price: 'Rs 150', img: spicy },
+  { id: 9, name: 'Thatthai', price: 'Rs 100', img: thathai},
+  { id: 10, name: 'Cream Roll', price: 'Rs 130', img: cream },
+  { id: 11, name: 'Ghee Cake', price: 'Rs 160', img: ghee },
+  { id: 12, name: 'Banana Cake', price: 'Rs 170', img: banana },
+  { id: 13, name: 'Jam Roll', price: 'Rs 140', img: jam },
 ];
 
 const Products = ({ title, id }) => {
