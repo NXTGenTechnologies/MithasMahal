bbbc788653309b8580f11305bd5ff2b9e98cffdd
sivapriyaa17 <sivapriyaa2006@gmail.com>
2025-08-16 22:45:37 +0530

    Update Footer.jsx

--- mithas-mahal/src/components/Footer.jsx
+++ mithas-mahal/src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white text-center py-8">
-      <p>&copy; 2025 Mcube Sweets. All rights reserved.</p>
+      <p>&copy; 2025 Mithas Mahal Sweets. All rights reserved.</p>
       <p><b>Address:</b> 123 Sweet Street, Chennai, Tamil Nadu, India</p>
-      <p><b>Phone:</b> +91 98765 43210 | <b>Email:</b> contact@mcubesweets.com</p>
+      <p><b>Phone:</b> +91 98765 43210 | <b>Email:</b> contact@mithassweets.com</p>
     </footer>
   );
 };
