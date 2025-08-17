bf3d672422784c00fe541e52bb6ea79a8a469a7d
sivapriyaa17 <sivapriyaa2006@gmail.com>
2025-08-17 19:24:30 +0530

    Update Hero.jsx

--- mithas-mahal/src/components/Hero.jsx
+++ mithas-mahal/src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import heroImage from '/images/all-sweets.webp';
+import heroImage from '/images/hero5.jpg';
 
 const Hero = () => {
   return (
@@ -7,7 +7,7 @@ const Hero = () => {
   className="w-full h-[600px] md:h-[500px] text-center text-black bg-cover bg-center"
   style={{ backgroundImage: `url(${heroImage})` }}
 >
-      <h1 className="text-4xl md:text-5xl font-serif text-blue-600 mb-4">Welcome to Mithas Mahal Sweets</h1>
+      <h1 className="text-4xl md:text-5xl font-serif text-white-600 mb-4">Welcome to Mithas Mahal Sweets</h1>
       <p className="text-lg md:text-xl">Delicious sweets, savories & bakes crafted with love</p>
     </section>
   );
