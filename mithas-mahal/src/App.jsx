3187f12b9f0ca9ee7f3ed3ba9da1b7125ef695f6
sivapriyaa17 <sivapriyaa2006@gmail.com>
2025-08-20 22:40:59 +0530

    Update App.jsx

--- mithas-mahal/src/App.jsx
+++ mithas-mahal/src/App.jsx
@@ -127,8 +127,8 @@ function App() {
 
        <Route path="/aboutus" element={<About />} />
        <Route path="/sweets" element={<Sweets />} />
-        <Route path="/savouries" element={<Savouries />} />
-        <Route path="/bakes" element={<Bakes />} />
+        <Route path="/savouries" element={<Sweets />} />
+        <Route path="/bakes" element={<Sweets />} />
 
             </Routes>
 
